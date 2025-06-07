e7f88189af6eff4b2d78d4981d64551dd74d7190
Shota Oniani <103844303+onianis@users.noreply.github.com>
2025-06-07 11:48:42 +0400

    Update index.js
    Inflate LOC

--- index.js
+++ index.js
@@ -5,17 +5,24 @@ resetButton = document.getElementById("resetButton");
 lapButton = document.getElementById("lapButton");
 lapsDisplayContainer = document.getElementById("lapsDisplayContainer");
 
-let milliseconds = 0, seconds = 0, minutes = 0, hours = 0, lapCounter = 1;
+let milliseconds = 0; 
+let seconds = 0;
+let minutes = 0; 
+let hours = 0;
+let lapCounter = 1;
 let timerStarted = false;
 lapsDisplayContainer.innerHTML = "";
 lapsDisplayContainer.style.opacity = 0;
 
-const zeroPad = (num, places) => String(num).padStart(places, '0')
+const zeroPad = (num, places) => String(num).padStart(places, '0');
 
-function fade(element) {
+function fade(element) 
+{
     var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1) {
+    var timer = setInterval(function () 
+        {
+        if (op <= 0.1) 
+        {
             clearInterval(timer);
             element.style.display = 'none';
         }
@@ -25,11 +32,14 @@ function fade(element) {
     }, 50);
 }
 
-function unfade(element) {
+function unfade(element) 
+{
     var op = 0.1;  // initial opacity
     element.style.display = 'block';
-    var timer = setInterval(function () {
-        if (op >= 1) {
+    var timer = setInterval(function () 
+        {
+        if (op >= 1) 
+        {
             clearInterval(timer);
         }
         element.style.opacity = op;
@@ -38,19 +48,23 @@ function unfade(element) {
     }, 10);
 }
 
-function timerTicker() {
+function timerTicker() 
+{
     milliseconds += 10;
 
-    if (milliseconds >= 990) {
+    if (milliseconds >= 990) 
+    {
         seconds++;
         milliseconds = 0;
     }
-    if (seconds >= 59) {
+    if (seconds >= 59)
+    {
         minutes++;
         seconds = 0;
         milliseconds = 0;
     }
-    if (minutes >= 60) {
+    if (minutes >= 60) 
+    {
         hours++;
         minutes = 0;
         seconds = 0;
@@ -59,21 +73,28 @@ function timerTicker() {
     timeLabel.textContent = (`${zeroPad(hours, 2)}:${zeroPad(minutes, 2)}:${zeroPad(seconds, 2)}.${zeroPad(milliseconds, 3)}`);
 }
 
-function timerTickerInterval() {
+function timerTickerInterval() 
+{
     timerStarted = true;
     setIntervalId = setInterval(timerTicker, 10);
 }
 
-startButton.onclick = function () {
-    if (!timerStarted) timerTickerInterval();
+startButton.onclick = function () 
+{
+    if (!timerStarted) 
+    {
+        timerTickerInterval();
+    }
 }
 
-stopButton.onclick = function () {
+stopButton.onclick = function () 
+{
     clearInterval(setIntervalId);
     timerStarted = false;
 }
 
-resetButton.onclick = function () {
+resetButton.onclick = function () 
+{
     if (timerStarted) clearInterval(setIntervalId);
     timerStarted = false;
     milliseconds = 0;
@@ -85,21 +106,34 @@ resetButton.onclick = function () {
     fade(lapsDisplayContainer);
 }
 
-lapButton.onclick = function () {
-    if (lapCounter === 1) {
-        lapsDisplayContainer.innerHTML = "<p id=\"lapsDisplayTitle\">Laps:</p><p>" + lapCounter +
-            ".&emsp;&emsp;&emsp;" + zeroPad(hours, 2) +
-            ":" + zeroPad(minutes, 2) +
-            ":" + zeroPad(seconds, 2) +
-            "." + zeroPad(milliseconds, 3) + "</p>";
+lapButton.onclick = function () 
+{
+    if (lapCounter === 1) 
+    {
+        lapsDisplayContainer.innerHTML = "<p id=\"lapsDisplayTitle\">Laps:</p><p>" + 
+            lapCounter +
+            ".&emsp;&emsp;&emsp;" + 
+            zeroPad(hours, 2) +
+            ":" + 
+            zeroPad(minutes, 2) +
+            ":" + 
+            zeroPad(seconds, 2) +
+            "." + 
+            zeroPad(milliseconds, 3) + "</p>";
         unfade(lapsDisplayContainer);
     }
-    else {
-        lapsDisplayContainer.innerHTML += "<p>" + lapCounter +
-            ".&emsp;&emsp;&emsp;" + zeroPad(hours, 2) +
-            ":" + zeroPad(minutes, 2) +
-            ":" + zeroPad(seconds, 2) +
-            "." + zeroPad(milliseconds, 3) + "</p>";
+    else 
+    {
+        lapsDisplayContainer.innerHTML += "<p>" + 
+            lapCounter +
+            ".&emsp;&emsp;&emsp;" + 
+            zeroPad(hours, 2) +
+            ":" + 
+            zeroPad(minutes, 2) +
+            ":" + 
+            zeroPad(seconds, 2) +
+            "." + 
+            zeroPad(milliseconds, 3) + "</p>";
     }
     lapCounter++;
 }
